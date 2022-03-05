add9fdcb3905cb590558ad4d01828f0c24d24776
iCharlieAraiza <eliptikman1234@gmail.com>
2022-03-05 07:50:14 -0600

    Init calendar first component

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import styled from 'styled-components'
+import { getMonth } from '../../utils/calendarUtils'
+import { Popup } from 'semantic-ui-react'
+import {AiFillInfoCircle} from 'react-icons/ai'
+import Calendar from '../../components/Calendar'
 
 const Main = () => {
+  console.table(getMonth(2))
+
   return (
-    <div>Main</div>
+    <TaskSection>
+      <CalendarSection>
+        <TaskSectionTitle>
+          Tasks <Popup content='In this section, you will manage all your scheduled tasks' trigger={<AiFillInfoCircle />} />
+        </TaskSectionTitle>
+        <Calendar />
+      </CalendarSection>
+    </TaskSection>
   )
 }
+
+const TaskSection = styled.div`
+  display: flex;
+  padding: 1.5rem 2rem;
+`
+
+const CalendarSection = styled.div`
+  flex-basis: 40rem;
+  flex-grow: 999;
+  min-width: 60%;
+`
+
+const TaskSectionTitle = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 1.5rem;
+
+  svg {
+    margin-left: 0.5rem;
+    opacity: 0.5;
+  }
+`
+
 
 export default Main
