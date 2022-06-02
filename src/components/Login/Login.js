ef6532cd4d10a1ce8b12cbb0e01568a20669b86e
iCharlieAraiza <eliptikman1234@gmail.com>
2022-06-01 21:51:07 -0500

    Update login styles

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,109 +1,151 @@
-import React, {useState} from 'react'
-import styled from 'styled-components'
-import { RiUserFill } from 'react-icons/ri'
-import { MdKeyboardArrowDown } from 'react-icons/md'
+    import React, {useState} from 'react'
+    import styled from 'styled-components'
+    import { RiUserFill } from 'react-icons/ri'
+    import { MdKeyboardArrowDown } from 'react-icons/md'
+    import { BiLogOut } from 'react-icons/bi'
+    import { AiOutlineUser } from 'react-icons/ai'
+    import { BsGearFill } from 'react-icons/bs'
 
+    const Login = () => {
+        const user = null
 
-const Login = () => {
-    const user = null
+        const [open, setOpen] = useState(false)
 
-    const [open, setOpen] = useState(false)
+        const handleClick = () => {
+            setOpen(!open)
+        }
 
-    return (
-        <Wrapper>
-            <UserCard onClick={() => setOpen(!open)} className={open && 'open'}>
-                <ProfileAvatar>
-                    <RiUserFill/>
-                </ProfileAvatar>    
-                <UserName>{user?.name != null? user.name : 'new user' }</UserName>
-                <SelectorArrow>
-                    <MdKeyboardArrowDown/>
-                </SelectorArrow>
-            </UserCard>
-            <MenuList className={open && 'open'}>
-                <Button>Sign Out</Button>
-            </MenuList> 
-        </Wrapper>
-    )
-}
-
-const Wrapper = styled.div`
-    position: relative;
-    width: 100%;
-`
-
-const UserCard = styled.div`
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-    cursor: pointer;
-    padding: 7px 6px;
-    border: 1px solid transparent;
-    border-radius: 5px;
-    &.open {
-        border: 1px #e0e0e047 solid;
-        background-color: #ffffff17;
+        return (
+            <>
+                <Wrapper>
+                    <UserCard onClick={handleClick} className={open && 'open'}>
+                        <ProfileAvatar>
+                            <RiUserFill/>
+                        </ProfileAvatar>    
+                        <UserName>{user?.name != null? user.name : 'Guest User' }</UserName>
+                        <SelectorArrow>
+                            <MdKeyboardArrowDown/>
+                        </SelectorArrow>
+                    </UserCard>
+                    <MenuList className={open && 'open'}>
+                        <Item onClick={handleClick}><AiOutlineUser/>My Profile</Item>
+                        <Item onClick={handleClick}><BsGearFill/>Settings</Item>
+                        <Separator/>
+                        <Item onClick={handleClick}> <BiLogOut/>Log Out</Item>
+                    </MenuList> 
+                </Wrapper>
+                {open && (<Overlay onClick={handleClick}/>)}
+            </>
+        )
     }
-`
 
-const ProfileAvatar = styled.div`
-    width: 2rem;
-    height: 2rem;
-    margin-right: 1rem;
-    border-radius: 50%;
-    background-color: #507fbb;
-    display: flex;
-    align-items: end;
-    justify-content: center;
-    overflow: hidden;
-    svg{
-        width: 80%;
-        height: 80%;
-        margin-bottom: -2px;
-    }
-`
+    const Wrapper = styled.div`
+        position: relative;
+        width: 100%;
+        z-index: 2;
+    `
 
-const UserName = styled.div`
-    text-transform: capitalize;
-`
+    const UserCard = styled.div`
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+        cursor: pointer;
+        padding: 7px 6px;
+        border: 1px solid transparent;
+        border-radius: 5px;
+        &.open {
+            border: 1px #e0e0e047 solid;
+            background-color: #ffffff17;
+        }
+    `
 
-const SelectorArrow = styled.div`
-    margin-left: auto;
-    svg {
-        font-size: 20px;
-    }
-`
+    const ProfileAvatar = styled.div`
+        width: 2rem;
+        height: 2rem;
+        margin-right: 1rem;
+        border-radius: 50%;
+        background-color: #507fbb;
+        display: flex;
+        align-items: end;
+        justify-content: center;
+        overflow: hidden;
+        svg{
+            width: 80%;
+            height: 80%;
+            margin-bottom: -2px;
+        }
+    `
 
-const MenuList = styled.div`
-    list-style: none;
-    padding: 0;
-    width: 100%;
-    left: 0;
-    position: absolute;
-    top: 0px;
-    backdrop-filter: blur(6px);
-    background-color: #818fa361;
-    top: 0px;
-    z-index: 0;
-    margin: 0;
-    transform: translateY(-200px);
-    transition: all 0.4s ease;
-    opacity: 0;
+    const UserName = styled.div`
+        text-transform: capitalize;
+    `
 
-    &.open {
-        z-index: 10;
-        display: block;
-        transform: translateY(100px);
-        opacity: 1;
-        top: 14px;
-    }
-    li {
+    const SelectorArrow = styled.div`
+        margin-left: auto;
+        svg {
+            font-size: 20px;
+        }
+    `
+
+    const MenuList = styled.div`
+        opacity: 0;
+        list-style: none;
+        padding: 0;
+        width: 100%;
+        left: 0;
+        position: absolute;
+        backdrop-filter: blur(6px);
+        background-color: #47536387;
+        //top: -2rem;
+        z-index: -10;
         margin: 0;
-    }
-`
+        transform: translateY(-2rem);
+        transition: all 0.15s ease;
+        box-shadow: 1px 1px 9px #00000080;
+        border-radius: 4px;
+        visibility: hidden;
+        &.open {
+            opacity: 1;
+            visibility: visible;
+            z-index: 10;
+            display: block;
+            transform: translateY(31px);
+            top: 18px;
+        }
+        li {
+            margin: 0;
+        }
+    `
 
-const Button = styled.div`
-    padding: 1rem;
-`
+    const Item = styled.div`
+        padding: 0.6rem;
+        display: flex;
+        align-items: center;
+        font-weight: 500;
+        font-size: 13px;
+        cursor: pointer;
+        svg {
+            margin-right: 0.5rem;
+        }
+        &:hover {
+            background-color: #0573e45e;
+        }
+    `
 
-export default Login
+    const Separator = styled.div`
+        width: 100%;
+        height: 1px;
+        background-color: #e6e6e638;
+    `
+
+    const Overlay = styled.div`
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        width: 100vw;
+        z-index: 1;
+    `
+
+    export default Login
